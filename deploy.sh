29c63a117639bd413b6e41eb3705a616ff6f6624
gquadros-dev <gabrielfernand464508@gmail.com>
2025-06-03 17:21:44 -0300

    ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
+git add -A
+git commit -m "$0"
+git push origin main
+
+ssh -p 2214 root@170.81.43.124
+cd ~/projetos/my-landing-page
+git pull origin main
+
 docker stop myLandingPage
 docker rm myLandingPage
 docker build -t my-landing-page .
 docker run -d --name myLandingPage -p 8080:80 -v ./src:/var/www/html/src my-landing-page
-
