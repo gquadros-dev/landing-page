32c25c33dd2018158883f014d194896de26e21b1
gquadros-dev <gabrielfernand464508@gmail.com>
2025-06-03 17:30:02 -0300

    teste

--- deploy.sh
+++ deploy.sh
@@ -39,6 +39,7 @@ echo "----------------------------------------------------"
 ssh -p "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST" << EOF
   echo "==> Conectado ao servidor: $REMOTE_HOST"
   echo "==> Navegando para o diretório do projeto: $PROJECT_DIR"
+  ls
   cd "$PROJECT_DIR" || { echo "Falha ao acessar o diretório $PROJECT_DIR no servidor"; exit 1; }
 
   #echo "==> Fazendo pull das últimas alterações do repositório..."
